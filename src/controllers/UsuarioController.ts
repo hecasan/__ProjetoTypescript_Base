3698b792cddcc1c8c84cb58a1da9b5598331cb8d
Hete Caetano <hetecaetano@gmail.com>
2022-03-28 22:28:10 -0300

    Criado o controller de usuários que vamos encaixar nas rotas

---
+++ src/controllers/UsuarioController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+class UsuarioController {
+  async findAll(req: Request, res: Response) {}
+
+  async findOne(req: Request, res: Response) {}
+
+  async create(req: Request, res: Response) {}
+
+  async update(req: Request, res: Response) {}
+
+  async destroy(req: Request, res: Response) {}
+}
+
+export default new UsuarioController();
