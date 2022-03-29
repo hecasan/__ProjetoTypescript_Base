6d97f0ed2336e413b0e72c50afa65e6c27c6b51a
Hete Caetano <hetecaetano@gmail.com>
2022-03-28 22:21:45 -0300

    Configurando middleware para interceptar um json passado no body da requisição com express.json()

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import { db } from "./database/db";
 import { router } from "./routes";
 
 const app = express();
+app.use(json);
 app.use(router);
 
 const port = 3000;
